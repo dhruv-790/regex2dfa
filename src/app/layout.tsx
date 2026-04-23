ff4e573bb029cf83ff28d2aa8f5c7c3eb1cef77b
dhruv-790 <dhruv79kumar@gmail.com>
2026-04-23 08:57:54 +0530

    analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type {Metadata, Viewport} from 'next';
 import './globals.css';
+import { Analytics } from "@vercel/analytics/react";
 
 export const viewport: Viewport = {
   width: 'device-width',
@@ -37,10 +38,12 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Space+Grotesk:wght@500;700&display=swap" rel="stylesheet" />
          
-         <script defer src="https://cloud.umami.is/script.js" data-website-id="ba18e9af-0e85-4d44-8a28-c41c2b5182c0"></script>
-      
+         
       </head>
-      <body className="font-body antialiased prevent-layout-shift">{children}</body>
+      <body className="font-body antialiased prevent-layout-shift">
+  {children}
+  <Analytics />
+</body>
     </html>
   );
 }
